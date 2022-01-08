45bdd048976f1294f1000d72f12c142a68af2e67
BryanBatuigas <91709090+BryanBatuigas@users.noreply.github.com>
2022-01-08 15:03:39 +0800

    .

---
+++ JAVASCRIPT/respondcss.js
@@ -0,0 +1,10 @@
+function sendMail(params) {
+	var tempParams = {
+		from_name:document.getElementById("from_email").value,
+		to_name:document.getElementById("to_email").value,
+		subject:document.getElementById("sub").value,
+		message:document.getElementById("msg").value,
+	};
+	emailjs.send('service_9mzjoac', 'template_110rix7', tempParams)
+	.then((message) => alert("Mail sent successfully"));
+	}
