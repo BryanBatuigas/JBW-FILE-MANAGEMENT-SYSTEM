bc10b1539b2a3be3faaf297f3b70f4923951829f
WendellMagdaluyo <91709079+WendellMagdaluyo@users.noreply.github.com>
2022-01-08 16:29:41 +0800

    .

--- JBW/JS_tools.js
+++ JBW/JS_tools.js
@@ -11,5 +11,19 @@ $(document).ready(function(){
 
 function error()
 {
-    window.print()
+    var cell = document.getElementById("row1");
+    var cell2 = document.getElementById("row2");
+    var cell3 = document.getElementById("row3");
+
+    if(cell.value =="" || cell2.value =="" || cell3.value =="")
+    {
+        alert("The table is empty!");
+    }
+    else
+    {
+        alert("The table is empty!");
+        window.print()
+
+    }
+
 }
