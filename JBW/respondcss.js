7f319cd6599ad2e88afe5e1608509e692ea7897b
WendellMagdaluyo <91709079+WendellMagdaluyo@users.noreply.github.com>
2022-01-08 15:32:57 +0800

    Update respondcss.js

--- JBW/respondcss.js
+++ JBW/respondcss.js
@@ -6,39 +6,27 @@ function sendMail(params) {
 		message:document.getElementById("msg").value,
 	};
 
-	if(from_name.value == "")
+	if(from_name.value == "" || to_name.value == "" || subject.value == "" || message.value == "")
 	{
-		alert("Please fill up the Sender email address");
-	}
-	else if(to_name.value == "")
-	{
-		alert("Please fill up the Receiver email address");
-	}
-	else if(subject.value == "")
-	{
-		alert("Please fill up the Subject");
-	}
-	else if(message.value == "")
-	{
-		alert("Please fill up the Message ");
+		alert("Please complete the form!");
 	}
 	else
 	{
-				
-	var userval = confirm("Are you sure you want to submit?");
-    if(userval == true){
-		emailjs.send('service_9mzjoac', 'template_110rix7', tempParams)
-		.then((message) => alert("Mail sent succesfully"));
-
-		document.getElementById("from_email").value = null;
-		document.getElementById("to_email").value = null;
-		document.getElementById("sub").value = null;
-		document.getElementById("msg").value = null;
-		
-	}
-
-	}
+		var userval = confirm("Are you sure you want to submit?");
+		if(userval == true){
+			emailjs.send('service_9mzjoac', 'template_110rix7', tempParams)
+			.then((message) => alert("Mail sent succesfully"));
 	
+			document.getElementById("from_email").value = null;
+			document.getElementById("to_email").value = null;
+			document.getElementById("sub").value = null;
+			document.getElementById("msg").value = null;
+			
+		}
+		
+	
+
+	}
 
 
 	}
