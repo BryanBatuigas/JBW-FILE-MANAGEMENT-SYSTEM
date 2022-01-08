62f9bb6c0e8a7dd9d2a80199a6388d2e66576a7a
WendellMagdaluyo <91709079+WendellMagdaluyo@users.noreply.github.com>
2022-01-08 15:28:16 +0800

    Update respondcss.js

--- JBW/respondcss.js
+++ JBW/respondcss.js
@@ -6,6 +6,23 @@ function sendMail(params) {
 		message:document.getElementById("msg").value,
 	};
 
+	if(from_name.value == "")
+	{
+		alert("Please fill up the Sender email address");
+	}
+	else if(to_name.value == "")
+	{
+		alert("Please fill up the Receiver email address");
+	}
+	else if(subject.value == "")
+	{
+		alert("Please fill up the Subject");
+	}
+	else if(message.value == "")
+	{
+		alert("Please fill up the Message ");
+	}
+
 	var userval = confirm("Are you sure you want to submit?");
     if(userval == true){
 		emailjs.send('service_9mzjoac', 'template_110rix7', tempParams)
